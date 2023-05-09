c7d592580a977ce5bd4f9fce510ae1f67b3f729c
xKronos58 <finleycrowther@outlook.com>
2023-05-09 20:21:32 +1000

    Initial Commit

---
+++ CodeBridge/Views/Pages/FullMainPage.xaml.cs
@@ -0,0 +1,103 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+
+namespace SideNav.Views.Pages;
+
+public partial class FullMainPage : Page
+{
+    public Dictionary<string, string> Members = new Dictionary<string, string>();
+
+    public FullMainPage()
+    {
+        InitializeComponent();
+        LoadMembers();
+    }
+
+    public void LoadMembers()
+    {
+        if (Members.Count == 0)
+            DefaultMembers();
+
+        List<Label> memberNames = new List<Label>();
+
+        foreach (var member in Members.Values)
+        {
+            Label label = new Label();
+            label.Content = member;
+            label.Margin = new Thickness(52, 30, 0, 0);
+            label.Width = 182;
+            label.Height = 35;
+            label.Foreground = Brushes.White;
+            label.Background = new SolidColorBrush(Color.FromRgb(20, 18, 39));
+            label.FontSize = 14;
+
+            // Create the style object
+            Style labelStyle = new Style(typeof(Label));
+            ControlTemplate template = new ControlTemplate(typeof(Label));
+            FrameworkElementFactory borderFactory = new FrameworkElementFactory(typeof(Border));
+            borderFactory.SetValue(Border.BackgroundProperty, new TemplateBindingExtension(Label.BackgroundProperty));
+            borderFactory.SetValue(Border.BorderBrushProperty, new TemplateBindingExtension(Label.BorderBrushProperty));
+            borderFactory.SetValue(Border.BorderThicknessProperty,
+                new TemplateBindingExtension(Label.BorderThicknessProperty));
+            borderFactory.SetValue(Border.CornerRadiusProperty, new CornerRadius(15));
+            FrameworkElementFactory contentPresenterFactory = new FrameworkElementFactory(typeof(ContentPresenter));
+            contentPresenterFactory.SetValue(ContentPresenter.ContentProperty,
+                new TemplateBindingExtension(Label.ContentProperty));
+            contentPresenterFactory.SetValue(ContentPresenter.MarginProperty,
+                new Thickness(8, 5, 0, 0)); // Add left padding
+            borderFactory.AppendChild(contentPresenterFactory);
+            template.VisualTree = borderFactory;
+            labelStyle.Setters.Add(new Setter(Label.TemplateProperty, template));
+            labelStyle.Setters.Add(new Setter(Label.EffectProperty, new DropShadowEffect()
+            {
+                Color = Color.FromRgb(20, 18, 39),
+                ShadowDepth = 2,
+                Opacity = 0.8,
+                BlurRadius = 20,
+                Direction = 270
+            }));
+
+            // Apply the style to the label control
+            label.Style = labelStyle;
+
+            memberNames.Add(label);
+        }
+
+        // Add all labels to the MemberGrid.Children collection at once
+        foreach (var label in memberNames)
+        {
+            MemberGrid.Children.Add(label);
+        }
+    }
+
+
+    private void ImageLoadFail(object? sender, ExceptionRoutedEventArgs e)
+    {
+        MessageBox.Show("ProfilePicture failed to load.");
+    }
+
+    #region defaults
+
+    public void DefaultMembers()
+    {
+        Members.Add("a000001a", "Mike");
+        Members.Add("a000001b", "John");
+        Members.Add("a000001c", "Luke");
+        Members.Add("a000001d", "Josh");
+        Members.Add("a000001e", "Mark");
+        Members.Add("a000001f", "Paul");
+        Members.Add("a000001g", "Peter");
+        Members.Add("a000001h", "James");
+        Members.Add("a000001i", "Jude");
+        Members.Add("a000001j", "Matthew");
+        Members.Add("a000001k", "Thomas");
+        Members.Add("a000001l", "Andrew");
+        Members.Add("a000001m", "Simon");
+    }
+
+    #endregion
+}
---
+++ CodeBridge/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.Xml;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+using Microsoft.Win32;
+using SideNav.Views.Pages;
+
+namespace SideNav.Views.Windows;
+
+public partial class MainWindow : Window
+{
+    private static bool isLoggedIn = false;
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+        if (isLoggedIn)
+            _LoginScreen();
+    }
+    
+    public static MainWindow mainWindow;
+    
+    //Login screen method
+    private void _LoginScreen()
+        => this.Content = mainWindow;
+
+    private void SignIn(object sender, RoutedEventArgs e)
+    {
+        if (StringFromRichTextBox(Username).ToLower() == "admin" || Password.Password == "123")
+            this.Content = new FullMainPage();
+        else
+            MessageBox.Show("Incorrect username or password.");
+    }
+    
+    string StringFromRichTextBox(RichTextBox rtb)
+    {
+        TextRange textRange = new TextRange(
+            rtb.Document.ContentStart,
+            rtb.Document.ContentEnd
+        );
+        return textRange.Text;
+    }
+
+}
